f18feab9416ca4ab4498f3ee24292c671986d5e5
Joacom7 <joacom7@gmail.com>
2022-08-22 18:56:10 -0300

    backend folder

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { contactsRouter } from './src/routers/contacts.router.js';
+import { contactsRouter } from './Backend/src/routers/contacts.router.js.js';
 
 const app = express();
 dotenv.config();
